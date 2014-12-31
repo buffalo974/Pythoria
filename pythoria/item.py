d11f3fab512d549804d0f36dd069e0ef50f6586b
buffalo974 <buffalo974@gmail.com>
2014-12-31 07:08:42 +0400

    utilisation de super(), legeres modifications et simplifications des classes, besoin d'un prototype de jeu pour ameliorer SimpleObjet

--- pythoria/item.py
+++ pythoria/item.py
@@ -2,10 +2,10 @@
 
 class Item():
 
-	def __init__(self, description='',name,typeItem,poid,place,usure):
-		self.description = "Classe mere pour obtenir les classes Vivres,Arme,Protection,Ecritures,SimpleObjet,ComplexObjet   => Attributs name , typeItem , poid, place, usure"
+	def __init__(self, name, poid, place, usure, description=''):
+	
+		self.description = "Classe mere pour obtenir les classes Vivres,Arme,Protection,Ecritures,SimpleObjet,ComplexObjet   => Attributs name , poid, place, usure"
 		self.name = name
-		self.typeItem = typeItem # ===> Vivres = aliment / boisson ; arme ; protection ; ecritures ;  materiel craftable ; artefact
 		
 		
 		self.poid = poid
@@ -20,167 +20,130 @@ class Item():
 		
 class Vivres(Item):
 
-	def __init__(self, description='',name,typeItem,poid,place,usure,typeVivres,SatieteBonus,HydratationBonus,UsureCoeff):
+	def __init__(self, name, poid, place, usure, description='', satiete_bonus, hydratation_bonus):
 	
 	
-		Item.__init__(self, description='',name,typeItem,poid,place,usure)
+		super(Vivres, self).__init__(self, name, poid, place, usure, description='')	
 	
-	
-		self.description = "Classe heritant de Item et engendrant la classe Mangeable et la classe Buveable  => Attributs typeVivres , SatieteBonus , HydratationBonus, place, UsureCoeff"
-		self.typeVivres = mangeable # par defaut :risque de pourrir, compact  ; buvable = necessite recipient,peu compacte,lourd.
-
-		self.SatieteBonus = SatieteBonus # par defaut 
-		self.HydratationBonus = HydratationBonus # par defaut : la pasteque rehydrate un peu , le jambon assoife.
-		self.UsureCoeff = 1 # par defaut ; les viandes auront un fort coefficient d'usure, mais sont tres rassasiantes. Les aliments sales se conservent mais donnent soif.
-
-
-
-
+		self.description = "Classe heritant de Item et engendrant la classe Mangeable et la classe Buveable  => Attributs type_vivres , satiete_bonus , hydratation_bonus, place, usure_coeff"
+		
+		self.satiete_bonus = satiete_bonus # par defaut 
+		self.hydratation_bonus = hydratation_bonus # par defaut : la pasteque rehydrate un peu , le jambon assoife.
+		
 
 	def get_description(self):
-		return description		
+		return self.description		
 
-
-		
 # ===============================================================================____ SE BATTRE ____=================================================================================
-
-
-
-
-
 class Arme(Item):
 
-	def __init__(self, description='',name,typeItem,poid,place,usure,degat,portee,conso_mana,conso_ammo):
+	def __init__(self, name, poid, place, usure, description='', degat, portee, precision, conso_mana, conso_ammo):
 	
-		Item.__init__(self, description='',name,typeItem,poid,place,usure)
+		
+		super(Arme, self).__init__(self, name, poid, place, usure, description='')
 		self.description = "Classe pour instancier les armes   => Attributs typeArme , degat , portee, conso_mana, conso_ammo"
 		self.typeArme = typeArme
-
-		# courte lame par defaut ; longue lame ; rapiere ;  lance ; javelot ; arc ; arbalete ; fouet ; fumigene ; hache ; marteau ; fleau
+		
 		self.degat = degat
 		self.portee = portee
+		self.precision = precision
 
 		self.conso_mana = conso_mana
 		self.conso_ammo = conso_ammo
 
 
 	def get_description(self):
-		return description
-
-
+		return self.description
 
 		
 class Protection(Item):
 
-	def __init__(self, description='',name,typeItem,poid,place,usure,typeProtection,anatomie,absorption):
+	def __init__(self, name, poid, place, usure, description='', anatomie, absorption, malus_dxt):
 	
-		Item.__init__(self, description='',name,typeItem,poid,place,usure)
+		super(Protection, self).__init__(self, name, poid, place, usure, description='')
+		
 		self.description = "Classe pour instancier les protections   => Attributs typeProtection , anatomie , absorption"
-		self.typeProtection = typeProtection
-		# courte lame par defaut ; maille , plaque , ecailles,mythril
+		
 		self.anatomie = anatomie 
 		# liste_equip_slot = ['tete','cou','main_D','main_G','poignet_D','poignet_G','carquoi','thorax','rachis','abdomen','jambe_D','jambe_G','pied_D','pied_G']
 		self.absorption = absorption 
-
-
+		self.malus_dxt = malus_dxt
 
 	def get_description(self):
-		return description	
+		return self.description	
 
 
 # ===============================================================================____ LE SAVOIR ET LA MAGIE ____===================================================================
 
-# frequence_ecritures = dict(list(frequence_livre.items()) + list(frequence_grimoire.items()) + list(frequence_parchemin.items()))
-
-
-
 class Ecriture(Item):
 
-	def __init__(self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading):
+	def __init__(self, name, poid, place, usure, description='', required_savoir, time_for_reading):
 	
 	
-		Item.__init__(self, description='',name,typeItem,poid,place,usure)
+		super(Ecriture, self).__init__(self, name, poid, place, usure, description='')
 		self.description = "Classe qui engendre les classes Livre, Grimoire et Parchemin  => Attributs titre"
-		self.titre = titre
-		self.txt = txt
-		self.RequiredSavoir = RequiredSavoir
-		self.TimeForReading = TimeForReading # Nombre de tour pour livre un livre et donc augmenter son savoir, ne pas lire quand on est poursuivi par des monstres !
 		
-
+		self.required_savoir = required_savoir
+		self.time_for_reading = time_for_reading # Nombre de tour pour livre un livre et donc augmenter son savoir, ne pas lire quand on est poursuivi par des monstres !
+		
 	def get_description(self):
-		return description
+		return self.description
 
 		
 class Livre(Ecriture):
 
-	def __init__(self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading,SavoirBonus):
+	def __init__(self, name, poid, place, usure, description='', required_savoir, time_for_reading, savoir_bonus):
 	
-		Ecriture.__init__(self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading)
-		self.description = "Classe pour instancier les livres    => Attributs contenu , TimeForReading , SavoirBonus"
+		super(Livre, self).__init__(self, name, poid, place, usure, description='', required_savoir, time_for_reading)
+		self.description = "Classe pour instancier les livres    => Attributs contenu , time_for_reading , savoir_bonus"
 		
-		self.SavoirBonus = SavoirBonus # Un tres bon livre est leger, rapide a lire, et offre un max de points de savoir ; le contre exemple est un annuaire telephonique. 
+		self.savoir_bonus = savoir_bonus # Un tres bon livre est leger, rapide a lire, et offre un max de points de savoir ; le contre exemple est un annuaire telephonique. 
 
 	def get_description(self):
-		return description
+		return self.description
 
 		
 class Grimoire(Ecriture):
 
-	def __init__(self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading,EcoleMagique,Sortilege,RequiredMana):
-		Ecriture.__init__(self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading)
-		self.description = "Classe pour instancier les grimoires    => Attributs EcoleMagique , Sortilege , RequiredSavoir, RequiredMana"
-		self.EcoleMagique = EcoleMagique
-		self.Sortilege = Sortilege
+	def __init__(self, name, poid, place, usure, description='', required_savoir, time_for_reading, ecole_magique, sortilege, required_mana):
+	
+		super(Grimoire, self).__init__(self, name, poid, place, usure, description='', required_savoir, time_for_reading)
 		
-		self.RequiredMana = RequiredMana
+		self.description = "Classe pour instancier les grimoires    => Attributs ecole_magique , sortilege , required_savoir, required_mana"
+		self.ecole_magique = ecole_magique
+		self.sortilege = sortilege
+		
+		self.required_mana = required_mana
 
 
 	def get_description(self):
-		return description
-
+		return self.description
 		
 		
 class Parchemin(Ecriture):
 
-	def __init__self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading,Incantation):
+	def __init__self(self, name, poid, place, usure, description='', required_savoir, time_for_reading, Incantation):
 	
-		Ecriture.__init__(self, description='',name,typeItem,poid,place,usure,titre,txt,RequiredSavoir,TimeForReading)
+		super(Parchemin, self).__init__(self, name, poid, place, usure, description='', required_savoir, time_for_reading)
 		self.description = "Classe pour instancier les parchemins    => Attributs Incantation "
 		self.Incantation = Incantation
 
-
 	def get_description(self):
-		return description
+		return self.description
 
 # ===============================================================================    Objets  simples   ===================================================================
-
-
-
 class SimpleObjet(Item):
 
-	def __init__(self, description=''):
+	def __init__(self, name, poid, place, usure, description='', clef, eclairage, outil):
 		
 		
-		Item.__init__(self, description='',name,typeItem,poid,place,usure)
+		super(SimpleObjet, self).__init__(self, description='', name, poid, place, usure)
 		self.description = "Classe pour instancier les objets basiques comme une clef     => Attributs ???? "
 		self.clef = clef
 		self.eclairage = eclairage # ex : torche
 		self.outil = outil
 		
-		
 
 	def get_description(self):
-		return description
+		return self.description
 
-# ===============================================================================    Objets  complexe   ===================================================================
-
-class ComplexObjet(Item):
-
-	def __init__(self, description=''):
-		self.description = "Classe pour instancier les objets complexes comme un tonneau rempli de poudre et de clous     => Attributs ???? "
-		# reflechir
-
-
-
-	def get_description(self):
-		return description
